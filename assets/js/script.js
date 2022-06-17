b24e8ba4dc1f171dc5ce598812b8cfc4cd75d5e7
Matt <mwhlavaty@gmail.com>
2022-06-16 20:08:00 -0700

    added search bar city/state input function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,109 @@
 
-// global variables
-var searchFormEl = document.querySelector("#search-form")
-var ingredInputEl = document.querySelector("#ingred-name")
-var resultsArea = document.querySelector("#results-area")
+// global variables for search
+var searchButtonEl = document.querySelector(".search-btn")
 
+// variables for location info
+let apiKey = "ffe65789d16418b39e33722ce53e0bb8"
+let locationName = "";
+let locationLat = "";
+let locationLon = "";
+
+// variables and array for all weather related info
+let currentTemp = "";
+let currentWeather = "";
+let weatherIcon = "";
+let weatherInfo = [];
+
+// *** potential variables for bells and whistles
+// var ingredInputEl = document.querySelector("#ingred-name")
+// var resultsArea = document.querySelector("#results-area")
 
 // handle submit event
-var formSubmitHandler = function (event) {
+function formSubmitHandler(event) {
     event.preventDefault();
-    var ingredName = ingredInputEl.value.trim()
-    if (ingredName) {
-        getCocktailData(ingredName)
+    var searchInputEl = document.querySelector("#search-city")
+    var locateArray = searchInputEl.value.split(/[ ,]+/);
+    console.log("array: ", locateArray)
+    if (locateArray) {
+        getLatLon(locateArray[0], locateArray[1], "")        
         // clear old content
-        ingredInputEl.value = "";
-        resultsArea.textContent = "";
+        searchInputEl.value = "";
     } else {
         alert("Please enter an ingredient.")
     }
 };
+
+// search will run this function first to grab the locations lat and lon
+function getLatLon(city, state, country) {
+    let geocodeApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=${apiKey}`
+
+    fetch(geocodeApi)
+        .then(function (response) {
+            if (!response.ok) {
+                console.log(response.json())
+                alert("failed to fetch weather data")
+            }
+
+            return response.json();
+        })
+        .then(function (data) {
+            locationName = data[0].name;
+            locationLat = data[0].lat;
+            locationLon = data[0].lon;
+
+            // after getting the lat and lon this function runs a fetch request to get the actual weather
+            getWeather();
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function getWeather() {
+    let weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${locationLat}&lon=${locationLon}&exclude={part}&appid=${apiKey}&units=imperial`
+
+    fetch(weatherApiUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                console.log(response.json())
+                alert("failed to fetch weather data")
+            }
+
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.current.temp);
+            console.log(data.current.weather[0].description)
+            console.log(data.current.weather[0].icon)
+
+            currentTemp = data.current.temp;
+            currentWeather = data.current.weather[0].description;
+            weatherIcon = data.current.weather[0].icon;
+
+            weatherInfo = [currentTemp, currentWeather, weatherIcon];
+
+            // after getting the weather info if statements decide which group of cocktails to display from
+            if (currentTemp < 60) {
+                //hotDrinks();
+            } else if (currentTemp < 80) {
+                //normalDrinks();
+            } else {
+                //coldDrinks();
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+searchButtonEl.addEventListener("click", formSubmitHandler)
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
 
 // pass search criteria to API
 var getCocktailData = function (ingredName) {
@@ -67,89 +153,3 @@ var displayCocktailData = function (ingredName, data) {
     }
 };
 
-searchFormEl.addEventListener("submit", formSubmitHandler)
-
-let apiKey = "ffe65789d16418b39e33722ce53e0bb8"
-
-// variables for location info
-let locationName = "";
-let locationLat = "";
-let locationLon = "";
-
-// variables and array for all weather related info
-let currentTemp = "";
-let currentWeather = "";
-let weatherIcon = "";
-let weatherInfo = [];
-
-
-// search will run this function first to grab the locations lat and lon
-function getLatLon(city, state, country) {
-    let geocodeApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=${apiKey}`
-
-    fetch(geocodeApi)
-        .then(function (response) {
-            if (!response.ok) {
-                console.log(response.json())
-                alert("failed to fetch weather data")
-            }
-
-            return response.json();
-        })
-        .then(function (data) {
-            locationName = data[0].name;
-            locationLat = data[0].lat;
-            locationLon = data[0].lon;
-
-
-            console.log(`lat: ${locationLat}, lon: ${locationLon}`)
-
-            // after getting the lat and lon this function runs a fetch request to get the actual weather
-            getWeather();
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-}
-
-function getWeather() {
-    let weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${locationLat}&lon=${locationLon}&exclude={part}&appid=${apiKey}&units=imperial`
-
-    fetch(weatherApiUrl)
-        .then(function (response) {
-            if (!response.ok) {
-                console.log(response.json())
-                alert("failed to fetch weather data")
-            }
-
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-            console.log(data.current.temp);
-            console.log(data.current.weather[0].description)
-            console.log(data.current.weather[0].icon)
-
-            currentTemp = data.current.temp;
-            currentWeather = data.current.weather[0].description;
-            weatherIcon = data.current.weather[0].icon;
-
-            weatherInfo = [currentTemp, currentWeather, weatherIcon];
-
-            // after getting the weather info if statements decide which group of cocktails to display from
-            if (currentTemp < 60) {
-                //hotDrinks();
-            } else if (currentTemp < 80) {
-                //normalDrinks();
-            } else {
-                //coldDrinks();
-            }
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-}
-
-// calling the starting function just for texting purposes. We can attach this function to the search button to get everything started.
-getLatLon();
